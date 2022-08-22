7015918e1447cb6108fbc39db934546b090516c5
sheriffkule <zeljko225@yahoo.com>
2022-08-22 22:33:13 +0200

    Styling navigation

--- App.js
+++ App.js
@@ -3,11 +3,15 @@ import {StyleSheet, StatusBar} from 'react-native';
 import CategoriesScreen from './screens/CategoriesScreen';
 
 export default function App() {
-  return <CategoriesScreen style={styles.container} />;
+  return (
+    <>
+      <StatusBar barStyle="light-content">
+        <CategoriesScreen style={styles.container} />
+      </StatusBar>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'green',
-  },
+  container: {},
 });
