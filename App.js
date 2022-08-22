e263319aed9f0d23d63bbf2179c44eeee863f52f
sheriffkule <zeljko225@yahoo.com>
2022-08-22 18:19:53 +0200

    added categories

--- App.js
+++ App.js
@@ -1,22 +1,13 @@
 import React from 'react';
-import {StyleSheet, Text, View, StatusBar} from 'react-native';
+import {StyleSheet, StatusBar} from 'react-native';
+import CategoriesScreen from './screens/CategoriesScreen';
 
 export default function App() {
-  return (
-    <>
-      <View style={styles.container}>
-        <Text>Meal App, Starting</Text>
-      </View>
-      <StatusBar barStyle="default" />
-    </>
-  );
+  return <CategoriesScreen style={styles.container} />;
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#0077ff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'green',
   },
 });
