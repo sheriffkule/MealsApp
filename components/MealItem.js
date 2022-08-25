8e573ec0b4e83aed39e17dab68c374c36da2d70f
sheriffkule <zeljko225@yahoo.com>
2022-08-25 15:53:23 +0200

    adding images and styling

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Pressable, Text, Image, StyleSheet, Platform} from 'react-native';
 
-function MealItem({title}) {
+function MealItem({title, imageUrl, duration, complexity, affordability}) {
   return (
-    <View style={styles.container}>
-      <Text style={styles.textContainer}>{title}</Text>
+    <View style={styles.mealItem}>
+      <Pressable
+        android_ripple={{color: 'grey'}}
+        style={({pressed}) => (pressed ? styles.buttonPressed : null)}>
+        <View style={styles.innerContainer}>
+          <View>
+            <Image source={{uri: imageUrl}} style={styles.image} />
+            <Text style={styles.title}>{title}</Text>
+          </View>
+          <View style={styles.details}>
+            <Text style={styles.detailItem}>{duration}m</Text>
+            <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
+            <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
+          </View>
+        </View>
+      </Pressable>
     </View>
   );
 }
@@ -12,11 +26,44 @@ function MealItem({title}) {
 export default MealItem;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  mealItem: {
+    margin: 12,
+    borderRadius: 8,
+    backgroundColor: 'white',
+    elevation: 4,
+    shadowColor: 'black',
+    shadowOpacity: 0.35,
+    shadowOffset: {width: 0, height: 2},
+    shadowRadius: 12,
+    overflow: Platform.OS === 'android' ? 'hidden' : 'visible',
   },
-  textContainer: {
-    flex: 1,
+  buttonPressed: {
+    opacity: 0.4,
+  },
+  innerContainer: {
+    borderRadius: 8,
+    overflow: 'hidden',
+  },
+  image: {
+    width: '100%',
+    height: 200,
+  },
+  title: {
+    fontWeight: 'bold',
+    textAlign: 'center',
+    fontSize: 20,
+    margin: 6,
+    color: 'black',
+  },
+  details: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    padding: 6,
+  },
+  detailItem: {
+    marginHorizontal: 4,
+    fontSize: 12,
     color: 'black',
   },
 });
