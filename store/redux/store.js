d5317855e887e02486811c8294c062fe36a0899f
sheriffkule <zeljko225@yahoo.com>
2022-08-29 11:50:50 +0200

    Redux slices

--- store/redux/store.js
+++ store/redux/store.js
@@ -1,5 +1,9 @@
 import {configureStore} from '@reduxjs/toolkit';
 
+import favouriteReducer from './favourites';
+
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    favouriteMeals: favouriteReducer,
+  },
 });
